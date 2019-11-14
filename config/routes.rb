64c1e1c73de92a050d1cd62c40f72f717e234eda
majovainilla <maria_reyes@me.com>
2019-11-14 14:33:56 -0600

    Edit root route to add uri

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'static_pages#home'
+  root to: 'static_pages/home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
