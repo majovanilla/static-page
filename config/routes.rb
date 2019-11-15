5c44f637282a1355fc78276c431f4ac3d884d684
majovainilla <maria_reyes@me.com>
2019-11-15 11:23:09 -0600

    Edit ruby version and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'static_pages/home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  root to: 'static_pages#home'
+  get 'static_pages/home', to: 'static_pages#home'
+  get 'static_pages/help', to: 'static_pages#help'
+  get 'static_pages/about', to: 'static_pages#about'
+  get 'static_pages/contact', to: 'static_pages#contact'
 end
