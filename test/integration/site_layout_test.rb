598843208738133c1ffbd73fbb155fe80779e3a1
majovainilla <maria_reyes@me.com>
2019-11-15 15:51:31 -0600

    Edit tests and views to include complete title

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,18 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'layout links of the webpage' do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    get signup_path
+    assert_select 'title', full_title('Sign up')
+    get contact_path
+    assert_select 'title', full_title('Contact')
+    end
 end
