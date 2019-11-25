c3c9992c039b750983b92a82a9b92c21d23710b1
majovainilla <maria_reyes@me.com>
2019-11-25 16:41:25 -0600

    Edit actionview gem to prevent vulnerability

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  
+
   root to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
