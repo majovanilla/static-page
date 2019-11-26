1f6b583068c5c4189826fdcd9439bd5ff76c8605
majovainilla <maria_reyes@me.com>
2019-11-26 12:20:32 -0600

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   root to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
